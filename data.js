efcb83fac04e0ddd00b2bf5ea860718045be5f48
Danish Mehboob <dm3243@WKS-511B-02.local>
2017-03-07 11:49:44 -0500

    new file to add the data elements

---
+++ data.js
@@ -0,0 +1,12 @@
+/**
+ * @author Danish Mehboob
+ */
+/*NEED TO CHANGE THE NAME CHART1DATA LATER
+ *I'm adding this variable to simplify the process of dealing with larger sets of data
+ */
+var chart1Data = [['Somali', 1405], ['Burmese', 658], ['Ethiopian', 277], ['Iraqi', 167], ['Bhutanese', 126]];
+
+/*NEED TO CHANGE THE NAME CHART2DATA LATER
+ *I'm adding this variable to simplify the process of dealing with larger sets of data
+ */
+var Chart2Data = [['Afghanistan', 23706, 'label1'], ['Albania', 104, 'label2'], ['Algeria', 94182, 'label3'], ['Angola', 15555, 'label4'], ['Anguilla', 1, 'label5'], ['Antigua and Barbuda', 15, 'label6'], ['Argentina', 3207, 'label7'], ['Armenia', 3319, ''], ['Aruba', 2, ''], ['Australia', 36917, ''], ['Austria', 72216, ''], ['Azerbaijan', 1278, ''], ['Bahamas', 8, ''], ['Bahrain', 247, ''], ['Bangladesh', 31958, ''], ['Barbados', 0, ''], ['Belarus', 1809, ''], ['Belgium', 35314, ''], ['Belize', 35, ''], ['Benin', 530, '']];
